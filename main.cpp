5a25709581c93a6cf2fe98febd85b19b24cca605
Zenenon <272938@student.pwr.edu.pl>
2025-04-02 16:40:03 +0200

    wersja na mutexach a nie spinlockach

--- main.cpp
+++ main.cpp
@@ -1,75 +1,87 @@
 #include <iostream>
-#include <chrono>
-#include <thread>
-#include <random>
-#include <vector>
-#include <atomic>
+#include <pthread.h>
+#include <unistd.h>
+#include <iomanip>
+
 using namespace std;
 
-int myrand(int min, int max) {
-    static mt19937 rnd(time(nullptr));
-    return uniform_int_distribution<>(min, max)(rnd);
-}
-
-class SpinLock {
-    atomic_flag flag = ATOMIC_FLAG_INIT;
+class Mutex {
+private:
+    volatile bool locked;
 public:
+    Mutex() : locked(false) {}
     void lock() {
-        while (flag.test_and_set(memory_order_acquire)) {
-            this_thread::yield();
+        while (__sync_lock_test_and_set(&locked, 1)) {
+            // Busy wait until unlocked
         }
     }
     void unlock() {
-        flag.clear(memory_order_release);
+        __sync_lock_release(&locked);
     }
 };
 
-void philosopher(int ph, SpinLock& ma, SpinLock& mb, SpinLock& mo) {
-    for (;;) {
-        int duration = myrand(200, 800);
-        {
-            mo.lock();
-            cout << ph << " thinks " << duration << "ms\n";
-            mo.unlock();
-        }
-        this_thread::sleep_for(chrono::milliseconds(duration));
+int num_philosophers;
+Mutex* forks;
+Mutex print_mutex;
 
-        {
-            mo.lock();
-            cout << "\t\t" << ph << " is hungry\n";
-            mo.unlock();
+void log_state(int id, const string& state, int duration = 0) {
+    print_mutex.lock();
+    if (state == "thinks" || state == "eats") {
+        cout << setw(id * 8) << "" << id + 1 << " " << state << " " << duration << "ms\n";
+    } else {
+        cout << setw(id * 8) << "" << id + 1 << " " << state << "\n";
+    }
+    print_mutex.unlock();
+}
+
+void* philosopher(void* arg) {
+    int id = *(int*)arg;
+    delete (int*)arg;
+
+    while (true) {
+        int think_time = (rand() % 300) + 200;
+        log_state(id, "thinks", think_time);
+        usleep(think_time * 1000);
+
+        log_state(id, "is hungry");
+        if (id % 2 == 0) {
+            forks[id].lock();
+            forks[(id + 1) % num_philosophers].lock();
+        } else {
+            forks[(id + 1) % num_philosophers].lock();
+            forks[id].lock();
         }
 
-        ma.lock();
-        mb.lock();
-        duration = myrand(200, 800);
-        {
-            mo.lock();
-            cout << "\t\t\t\t" << ph << " eats " << duration << "ms\n";
-            mo.unlock();
-        }
-        this_thread::sleep_for(chrono::milliseconds(duration));
-        ma.unlock();
-        mb.unlock();
+        int eat_time = (rand() % 600) + 200;
+        log_state(id, "eats", eat_time);
+        usleep(eat_time * 1000);
+
+        forks[id].unlock();
+        forks[(id + 1) % num_philosophers].unlock();
     }
 }
 
 int main() {
-    cout << "Dining Philosophers C++11 with Resource hierarchy\n";
+    cout << "Enter the number of philosophers: ";
+    cin >> num_philosophers;
 
-    int n;
-    cout << "Enter number of philosophers: ";
-    cin >> n;
-
-    vector<SpinLock> forks(n);
-    SpinLock mo;
-    vector<thread> philosophers;
-
-    for (int i = 0; i < n; ++i) {
-        philosophers.emplace_back([&, i] { philosopher(i + 1, forks[i], forks[(i + 1) % n], mo); });
+    if (num_philosophers < 2) {
+        cerr << "There must be at least 2 philosophers." << endl;
+        return 1;
     }
 
-    for (auto& t : philosophers) {
-        t.join();
+    pthread_t threads[num_philosophers];
+    forks = new Mutex[num_philosophers];
+
+    for (int i = 0; i < num_philosophers; i++) {
+        int* id = new int(i);
+        pthread_create(&threads[i], nullptr, philosopher, id);
     }
+
+    for (int i = 0; i < num_philosophers; i++) {
+        pthread_join(threads[i], nullptr);
+    }
+
+    delete[] forks;
+    return 0;
 }
